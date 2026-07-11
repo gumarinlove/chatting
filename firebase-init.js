796336622ebea03027f97a15e64d9de419991ef5
gumarinlove <gumarin1024@gmail.com>
2026-07-11 11:31:53 +0900

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,9 +1,8 @@
-// 1. Firebase SDK 가져오기 (CDN 방식)
+// firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js";
 
-// 2. 전달해주신 설정 코드
 const firebaseConfig = {
   apiKey: "AIzaSyBTPc4Jdtp5bCRR6Sl2KnBARG88zAb4dco",
   authDomain: "chatting-251e5.firebaseapp.com",
@@ -13,7 +12,6 @@ const firebaseConfig = {
   appId: "1:896657317592:web:456b81e34b290d6a89c1ad"
 };
 
-// 3. 앱 초기화 및 내보내기 (다른 파일에서 쓰기 위함)
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
